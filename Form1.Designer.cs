d56bab766a086d06939eab24a18499061d43f769
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-08-05 12:28:40 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Golf
+{
+    partial class Golf_database
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvGolf = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGolf)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvGolf
+            // 
+            this.dgvGolf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGolf.Location = new System.Drawing.Point(12, 12);
+            this.dgvGolf.Name = "dgvGolf";
+            this.dgvGolf.Size = new System.Drawing.Size(750, 290);
+            this.dgvGolf.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(33, 331);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(122, 43);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // Golf_database
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvGolf);
+            this.Name = "Golf_database";
+            this.Text = "Golf Database";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGolf)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvGolf;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Golf
+{
+    public partial class Golf_database : Form
+    {
+        //
+        private string connectionString = @"Data Source=DPKASTG-05\SQLEXPRESS;Initial Catalog=golf;Integrated Security=True";
+        SqlConnection Con = new SqlConnection();
+        DataTable GolfTable = new DataTable();
+
+
+
+
+        public Golf_database()
+        {
+            InitializeComponent();
+            Con.ConnectionString = connectionString;
+            loaddb();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void loaddb()
+        {
+            //Load columns in DataTable
+            datatablecolumns();
+        }
+
+        public void datatablecolumns()
+        {
+            //Clear old data
+            GolfTable.Clear();
+
+            //add column titles in the datatable
+            try
+            {
+                GolfTable.Columns.Add("ID");
+                GolfTable.Columns.Add("Title");
+                GolfTable.Columns.Add("FirstName");
+                GolfTable.Columns.Add("Surname");
+                GolfTable.Columns.Add("Gender");
+                GolfTable.Columns.Add("DOB");
+                GolfTable.Columns.Add("Street");
+                GolfTable.Columns.Add("Suburb");
+                GolfTable.Columns.Add("City");
+                GolfTable.Columns.Add("Available Week Days");
+                GolfTable.Columns.Add("Handicap");
+            }
+            catch
+            {
+                MessageBox.Show("Data Table not loaded");
+            }
+
+        }
+    }
+}
